1ea150784a9c0a107fbf78ea9e46fa93941334cd
nish124 <ndhere@grepruby.io_>
2020-09-13 16:28:11 +0530

    done time conversion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import * as moment from 'moment';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'timeConversionApp';
+
+  minutData;
+  secondData;
+
+  flagHour=false;
+  flagMin=false;
+
+  constructor(){  }
+
+  data;
+  datasecond;
+
+    getTimeFromMins(mins) {
+      this.flagHour=true;
+      this.data = moment.utc().startOf('day').add(mins, 'minutes').format('H');
+      console.log(this.data);
+  }
+    getTimeFromSecond(sec) {
+      this.flagMin=true;
+        this.datasecond = moment.utc().startOf('day').add(sec, 'second').format('mm');
+        console.log(this.datasecond);
+    }
 }
